06f8d4cf65842604aa4478770b15c9032644e6a7
Gustavo Martins <gugumartin77@gmail.com>
2022-03-31 16:25:53 -0300

    C7

--- src/controllers/Jogo.Controller.js
+++ src/controllers/Jogo.Controller.js
@@ -1,9 +1,31 @@
+const res = require("express/lib/response");
 const Jogo = require("../models/Jogo");
 
 const getAll = async (req, res) => {
   try {
     const jogos = await Jogo.findAll();
-    res.render("index", {jogos});
+    res.render("index", { jogos });
+  } catch (err) {
+    res.status(500).send({ err: err.message });
+  }
+};
+
+const cadastro = (req, res) => {
+  try {
+    res.render("cadastro");
+  } catch (err) {
+    res.status(500).send({ err: err.message });
+  }
+};
+
+const create = async (req, res) => {
+  try {
+    const jogo = req.body;
+    if (!jogo) {
+      return res.redirect("/cadastro");
+    }
+    await Jogo.create(jogo);
+    res.redirect("/");
   } catch (err) {
     res.status(500).send({ err: err.message });
   }
@@ -11,4 +33,6 @@ const getAll = async (req, res) => {
 
 module.exports = {
   getAll,
+  cadastro,
+  create,
 };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,5 +2,7 @@ const routes = require("express").Router();
 const JogoController = require("../controllers/Jogo.Controller");
 
 routes.get("/", JogoController.getAll);
+routes.get("/cadastro", JogoController.cadastro);
+routes.post("/create", JogoController.create);
 
 module.exports = routes;
