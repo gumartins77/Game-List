94208e9bbe40305d5a4347b31a773a5f38386d39
Gustavo Martins <gugumartin77@gmail.com>
2022-04-01 15:15:52 -0300

    C11 init table

--- src/models/Jogo.js
+++ src/models/Jogo.js
@@ -1,48 +1,56 @@
 const Sequelize = require("sequelize");
 const connection = require("../database/db");
 
-const Jogo = connection.define("jogo", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
+const Jogo = connection.define(
+  "jogo",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    nome: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    genero: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    descricao: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    plataforma: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    lancamento: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    estudio: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    imagem: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
   },
-  nome: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  genero: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  descricao: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  plataforma: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  lancamento: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  estudio: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  imagem: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-}, 
-{
+  {
     freezeTableName: true,
     timestamps: false,
     createdAt: false,
     updatedAt: false,
-}
+  }
 );
+
+const initTable = async () => {
+  await Jogo.sync();
+};
+
+initTable();
 
 module.exports = Jogo;
