7608196bb8d63d54df382415b028e4c2699b68ee
Gustavo Martins <gugumartin77@gmail.com>
2022-04-01 11:33:56 -0300

    C8

--- src/controllers/Jogo.Controller.js
+++ src/controllers/Jogo.Controller.js
@@ -4,7 +4,7 @@ const Jogo = require("../models/Jogo");
 const getAll = async (req, res) => {
   try {
     const jogos = await Jogo.findAll();
-    res.render("index", { jogos });
+    res.render("index", { jogos, jogoPut: null, jogoDel: null });
   } catch (err) {
     res.status(500).send({ err: err.message });
   }
@@ -31,8 +31,46 @@ const create = async (req, res) => {
   }
 };
 
+const getById = async (req, res) => {
+  try {
+    const method = req.params.method;
+    const jogos = await Jogo.findAll();
+    const jogo = await Jogo.findByPk(req.params.id);
+
+    if (method == "put") {
+      res.render("index", { jogos, jogoPut: jogo, jogoDel: null });
+    } else {
+      res.render("index", { jogos, jogoPut: null, jogoDel: jogo });
+    }
+  } catch (err) {
+    res.status(500).send({ err: err.message });
+  }
+};
+
+const update = async (req, res) => {
+  try {
+    const jogo = req.body;
+    await Jogo.update(jogo, { where: { id: req.params.id } });
+    res.redirect("/");
+  } catch (err) {
+    res.status(500).send({ err: err.message });
+  }
+};
+
+const remove = async (req, res) => {
+  try {
+    await Jogo.destroy({ where: { id: req.params.id } });
+    res.redirect("/");
+  } catch (err) {
+    res.status(500).send({ err: err.message });
+  }
+};
+
 module.exports = {
   getAll,
   cadastro,
   create,
+  getById,
+  update,
+  remove,
 };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,5 +4,8 @@ const JogoController = require("../controllers/Jogo.Controller");
 routes.get("/", JogoController.getAll);
 routes.get("/cadastro", JogoController.cadastro);
 routes.post("/create", JogoController.create);
+routes.get("/getById/:id/:method", JogoController.getById);
+routes.post("/update/:id", JogoController.update);
+routes.get("/remove/:id", JogoController.remove);
 
 module.exports = routes;
